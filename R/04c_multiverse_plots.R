e1b2f3b8a28e1871a245fcfccf51ebef4ecc3f22
Benjamin Listyg <listyg.ben@gmail.com>
2025-02-03 22:12:59 -0500

    Update 04c_multiverse_plots.R

--- R/04c_multiverse_plots.R
+++ R/04c_multiverse_plots.R
@@ -7,29 +7,29 @@ library(tidylog)
 library(patchwork)
 library(augsynth)
 library(furrr)
+
 pacman::p_load(geofacet)
 pacman::p_load(unglue)
 dir.create(here::here("combined_multiverse"))
+
 # List files and parse data
 mm <- list.files(here::here('multiverse_tables'),full.names = T)
 
 outcomes<- map_dfr(mm,read_csv)
+
 outcomes %>% write_csv(here::here("combined_multiverse/augsynth_multiverse.csv"))
+
 for (state in unique(outcomes$Level)){
+  
   print(state)
 
-outcomes %>% filter(Level==state)  %>% 
-  ggplot(aes(y=ATT,x = treatment))+
-  geom_violin()+
-  facet_wrap(~keyword) +
-  labs(title = str_glue('Multiverse_{state}'))
-ggsave(here::here(str_glue('figures/multiverse_{state}.png')),width=9,height=9)
+  outcomes %>% 
+    filter(Level==state) %>% 
+    ggplot(aes(y=ATT,x = treatment)) +
+    geom_violin() +
+    facet_wrap(~keyword) +
+    labs(title = str_glue('Multiverse_{state}'))
+  
+  ggsave(here::here(str_glue('figures/multiverse_{state}.png')),width=9,height=9)
+
 }
-
-outcomes %>% filter(Level=='Average') %>% count(treatment,keyword)
-
-outcomes %>% filter(Level=='Average') %>% filter(keyword=='pornhub') %>% summarise(min(CATT),max(CATT))
-outcomes %>% filter(Level=='Average') %>% filter(keyword=='xvideos') %>% summarise(min(CATT),max(CATT))
-outcomes %>% filter(Level=='Average') %>% filter(keyword=='vpn') %>% summarise(min(CATT),max(CATT))
-outcomes %>% filter(Level=='Average') %>% filter(keyword=='porn') %>% summarise(min(CATT),max(CATT))
-outcomes %>% count(scm)
