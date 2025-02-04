3594f1a1d4bed48ff95cae3f4a2589a307dd4f1a
Benjamin Listyg <listyg.ben@gmail.com>
2025-02-03 22:16:25 -0500

    Update 06c_multiverse_gsynth_plots.R

--- R/06c_multiverse_gsynth_plots.R
+++ R/06c_multiverse_gsynth_plots.R
@@ -7,36 +7,32 @@ library(tidylog)
 library(patchwork)
 library(augsynth)
 library(furrr)
+
 pacman::p_load(geofacet)
 pacman::p_load(unglue)
 dir.create(here::here("combined_multiverse"))
+
 # List files and parse data
+
 mm <- list.files(here::here('gsynth_tables'),full.names = T)
 
-outcomes<- map_dfr(mm,read_csv)
+outcomes <- map_dfr(mm,read_csv)
 
-outcomes %>% count(Level)
-state<-'Average'
+outcomes %>% write_csv(here::here("combined_multiverse/gsynth_results.csv"))
+
 for (state in unique(outcomes$Level)){
+  
   print(state)
-
-outcomes %>% filter(Level==state)  %>% 
-  filter(
-    (Time %in%c(4,13) &start_date=='2022-01-01')|
-    (Time %in%c(1,3) &start_date=='2019-01-01')
-  ) %>% write_csv(here::here("combined_multiverse/gsynth_results.csv"))
-  ggplot(aes(y=ATT,x = treatment))+
-  geom_violin()+
-  facet_wrap(~keyword) +
-  labs(title = str_glue('Multiverse_{state}'))
-ggsave(here::here(str_glue('figures/multiverse_gsynth_{state}.png')),width=9,height=9)
+  
+  outcomes %>% filter(Level==state)  %>% 
+    filter(
+      (Time %in%c(4,13) &start_date=='2022-01-01')|
+        (Time %in%c(1,3) &start_date=='2019-01-01')
+    ) %>% 
+    ggplot(aes(y=ATT,x = treatment))+
+    geom_violin()+
+    facet_wrap(~keyword) +
+    labs(title = str_glue('Multiverse_{state}'))
+  
+  ggsave(here::here(str_glue('figures/multiverse_gsynth_{state}.png')),width=9,height=9)
 }
-
-outcomes %>% filter(Level=='Average') %>% count(treatment,keyword)
-
-outcomes %>% filter(Level=='Average') %>% filter(keyword=='pornhub') %>% summarise(min(ATT),max(ATT))
-outcomes %>% filter(Level=='Average') %>% filter(keyword=='xvideos') %>% summarise(min(ATT),max(ATT))
-outcomes %>% filter(Level=='Average') %>% filter(keyword=='vpn') %>% summarise(min(ATT),max(ATT))
-outcomes %>% filter(Level=='Average') %>% filter(keyword=='porn') %>% summarise(min(ATT),max(ATT))
-outcomes %>% count(scm)
-outcomes %>% count(start_date)
