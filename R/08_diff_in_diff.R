39570cda264187fa613f6950481388112c03ace8
davidnathanlang <43049171+davidnathanlang@users.noreply.github.com>
2025-06-16 00:03:20 -0700

    need to figure out what is going with did_simple

--- R/08_diff_in_diff.R
+++ R/08_diff_in_diff.R
@@ -14,16 +14,20 @@ pacman::p_load(did)
 pacman::p_load(ptetools)
 pacman::p_load(staggered)
 # Create directories for saving outputs
-figures_dir <- here("figures")
-models_dir <- here("mods")
+
+figures_dir <- here("did_figures")
+
 dir.create(figures_dir, showWarnings = FALSE)
 dir.create(models_dir, showWarnings = FALSE)
-keyword<-'xvideos'
+
+
 seed<-12345
 inference<-'parametric'
 time_range <- '2022-01-01 2024-10-31'
 
-
+keyword<-'vpn'
+for (keyword in c('pornhub','vpn','xvideos','porn'))
+{
 keyword_df <- read_csv(here::here(str_glue("data/{keyword}.csv"))) %>%
   filter(time == time_range) # Adjust date range as needed
 state_num<-keyword_df %>% distinct(state) %>% mutate(state_num=row_number())
@@ -94,17 +98,20 @@ group_time_treatment %>%
     strip.text = element_text(face = "bold"),
     panel.grid.minor = element_blank()
   )
+ggsave(here::here(figures_dir,str_glue('{keyword}_grouptime.jpg')),width = 8,height = 10)
+
 
 
 dyn_did<-aggte(did_res,type='dynamic',min_e=-Inf,max_e = 84,na.rm = TRUE) %>% ggdid()
 dyn_did$data
 dyn_did$data$year<-dyn_did$data$year/7
 dyn_did+scale_x_continuous()
+ggsave(here::here(figures_dir,str_glue('{keyword}_eventstudy.jpg')),width = 8,height = 10)
 gg_group<-aggte(did_res,type='group',min_e=-Inf,max_e=84,na.rm=TRUE)
-ggdid(gg_group)
-?aggte()
-aggte(did_res,type='group',min_e = 91,na.rm=TRUE)
+#ggdid(gg_group)
 
+aggte(did_res,type='group',min_e = 84,na.rm=TRUE)
+aggte(did_res,type='group',na.rm=TRUE)
 
 
 pte_df %>% distinct(first_treat,state) %>%
@@ -112,11 +119,11 @@ pte_df %>% distinct(first_treat,state) %>%
   summarise(states=reduce(state,str_c(sep=' , ')))
 gg<-ggdid(dyn_group)
 
-gg$data
-dyn_simple<-aggte(did_res,type='simple',min_e=-Inf,max_e = 84,bstrap = TRUE,cband=TRUE,na.rm = TRUE)
-dyn_simple$overall.se*1.96+dyn_simple$overall.att
-str(dyn_simple)
+
+(dyn_simple<-aggte(did_res,type='simple',bstrap = TRUE,cband=TRUE,na.rm = TRUE))
+
 ?aggte
+
 dyn_simple$overall.att
 plot_data<-gg$data %>% left_join(states_treated,by=c('year'='first_treat')) %>%
   mutate(dates_plus_states=str_c(as_date(year), states_concat))
@@ -129,320 +136,7 @@ plot_data %>%
   geom_errorbar(aes(ymin = att-att.se*c,ymax=att+att.se*c,y=att))  +coord_flip()+
   geom_hline(aes(yintercept = 0))+
   labs(title = keyword_df$term)
-
-
-dyn_group$egt %>% left_join(states_treated,by=c('Group'='states_concat'))
-ggdid(dyn_did) +
-  scale_x_date(
-    date_breaks = "2 weeks",           # or "1 month", depending on your data
-    date_labels = "%b %d"              # e.g., Jan 01, or use "%Y-%m" for year-month
-  ) 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  labs(x = "Date", y = "Average Effect", color = "Group")
-ggdid(dyn_simple)
-dyn
-did_res
-did_res2 <- did::att_gt(
-  yname = "hits",
-  gname = "first_treat",
-  tname = "date",
-  idname = "state_lookup",
-  data = pte_df %>% left_join(pte_labels)
-  
-)
-mod<-summary(dyn_simple)
-
-remotes::install_github("tjhon/ssynthdid")
-pacman::p_load(bacondecomp)
-pte_labels<-pte_df %>% distinct(state) %>% mutate(state_lookup=row_number())
-fixest::feols(hits~sunab(post_treat|state+date|state,data=pte_df))
-pacman::p_load(fixest)
-staggered(df=pte_df,
-          y="hits",
-          g='first_treat',
-          t='date',
-          i='state',
-          estimand = 'simple'
-          
-          )
-summary(did_res)
-ggpte(did_res)
-df <- staggered::pj_officer_level_balanced #load the officer data
-
-Simple aggregate parameters
-data(mdpta)
-# Hyperparameter search function
-hyperparameter_search <- function(keyword, time_range = '2022-01-01 2024-10-31', seed = 12345, inference = "parametric") {
-  # Load data
-
-  pte(hits,geo,date,)
-  keyword_df
-  
-  # Model fitting
-  mod <- gsynth(
-    hits ~ post_treat,
-    data = keyword_df,
-    index = c("state", "int_date"),
-    se = TRUE,
-    seed = seed,
-    inference = inference,
-    force = 'none'
-  )
-  
-  # Cumulative effects
-  ce_effects <- cumuEff(mod, period = c(0, 12))$est.catt %>%as.tibble()%>% mutate(topic=keyword) %>% mutate(rn=row_number()-1)
-  ce_effects
-  # Save model output
-  saveRDS(mod, file = here(models_dir, str_glue("{keyword}_model.rds")))
-  three_month_att<- ce_effects %>% filter(rn+1==13) %>% transmute(ATT=CATT/13) %>% pull()
-  
-  # Generate plots
-  att_plot <- plot(mod) + labs(title = str_glue("{keyword} ATT")) +
-    geom_hline(yintercept = three_month_att,linetype='dashed')+
-    annotate("text", x = -12, y = three_month_att+2, label = str_c("ATT = ",round(three_month_att,1)))
-  
-  treated_states <- keyword_df %>%
-    filter(post_treat == 1) %>%
-    distinct(state) %>%
-    pull(state)
-  
-  # Generate plots for each state and store in a list
-  state_plots <- lapply(treated_states, function(state) {
-  
-    thre_month_est<-cumuEff(mod,cumu = FALSE,id=state,c(0,12)) %>% as_tibble() %>% summarise(three_month_att=mean(catt)) %>% pull(three_month_att)
-    plot(mod, id = state) + 
-      labs(
-        x = "Time (Weeks) ",  # New x-axis label
-        y = "ATT", # (Average Treatment Effect)",     # New y-axis label
-        title = str_glue("{state}") # Unique title for each plot
-      ) +
-      geom_hline(yintercept = thre_month_est,linetype='dashed')+
-      annotate("text", x = -25, y = thre_month_est+2, label = str_c("ATT = ",round(thre_month_est,1))) +
-      theme_minimal() # Optional: Use a clean theme
-  })
-  
-  # Combine all plots into a single layout using patchwork
-  combined_plot <- wrap_plots(state_plots) +
-    plot_annotation(title = glue("All States: {keyword}")) +
-    theme(plot.title = element_text(hjust = 0.5))
-  
-  # Display the combined plot
-  # combined_plot
-  
-  
-  # Save figures
-  ggsave(filename = here(figures_dir, str_glue("{keyword}_state_plots.png")), plot = combined_plot,width = 9,height = 9)
-  ggsave(filename = here(figures_dir, str_glue("{keyword}_att_plot.png")), plot = att_plot,width = 9,height = 9)
-  
-  vline_dates<-keyword_df %>% distinct(enforcement_date,state) %>% drop_na()
-  # Overall keyword plot
-  overall_plot <- keyword_df %>%
-    ggplot(aes(x = date, y = hits,colour = factor(post_treat))) +
-    geom_vline( aes(xintercept = enforcement_date),data=vline_dates,linetype='dashed') +
-    geom_line() +
-    #geom_point()+
-    facet_geo(~state) +
-    labs(
-      title = str_glue("{keyword}"),
-      colour = "Age Verification Status" # Update legend title
-    ) +
-    scale_colour_manual(
-      values = c("0" = "red", "1" = "blue"), # Customize colors if desired
-      labels = c("0" = "Age Verification Laws Not Passed", "1" = "Age Verification Passed")
-    ) +
-    scale_x_date(
-      date_labels = "'%y", # Display last two digits of the year
-      date_breaks = "1 year" # Adjust breaks as necessary
-    ) +
-    theme_minimal() + # Use a cleaner theme 
-    theme(legend.position = 'bottom')
-  
-  
-  ggsave(filename = here(figures_dir, str_glue("{keyword}_overall_plot.png")), plot = overall_plot,width = 9,height = 9)
-  
-  list(mod = mod, cum_effects = ce_effects, att_plot = att_plot, state_plots = combined_plot, overall_plot = overall_plot,search_term=keyword)
+ggsave(here::here(figures_dir,str_glue('{keyword}_state_agg.jpg')))
 }
 
-library(staggered) #load the staggered package
-library(ggplot2)   #load ggplot2 for plotting the results
-#> Warning: package 'ggplot2' was built under R version 4.3.1
 
-df <- staggered::pj_officer_level_balanced #load the officer data
-# Run hyperparameter search for multiple keywords
-keywords <- c("pornhub", "vpn", "xvideos", "porn")
-results <- lapply(keywords, function(keyword) {
-  hyperparameter_search(keyword = keyword)
-})
-
-# Access and display results
-# lapply(results, function(res) {
-#  print(summary(res$mod))
-#  res$cum_effects
-# })
-
-calculate_pct_change <- function(result, mod_index) {
-  plot(result[[mod_index]]$mod, type = 'counterfactual')$data %>%
-    pivot_wider(names_from = type, values_from = outcome) %>%
-    filter(time >= 0) %>%
-    mutate(pct_change = cumsum(tr - co) / cumsum(co)) %>% mutate(topic=result[[mod_index]]$search_term,rn=time)
-}
-
-pct_change<-bind_rows(
-  calculate_pct_change(results, 1),
-  calculate_pct_change(results, 2),
-  calculate_pct_change(results, 3),
-  calculate_pct_change(results, 4)
-)
-
-
-ce_pre_registered<-
-  bind_rows(
-    results[[1]]$cum_effects ,
-    results[[2]]$cum_effects ,
-    results[[3]]$cum_effects ,
-    results[[4]]$cum_effects
-  ) %>%  mutate(
-    time_point = case_when(
-      rn == 4 ~ "1 Month",
-      rn == 12 ~ "3 Months"
-    )) %>% 
-  left_join(pct_change,by=c('topic','rn'))
-
-#ce_pre_registered %>% 
-ce_fig <- ce_pre_registered %>% 
-  filter(rn %in% c(4, 12)) %>% 
-  mutate(topic = factor(topic, levels = c("pornhub", "xvideos", "vpn", "porn"))) %>%
-  ggplot(aes(x = time_point, y = CATT, fill = topic)) +
-  geom_bar(stat = "identity", position = position_dodge(width = 0.8), width = 0.6,alpha=1) +
-  geom_errorbar(aes(ymin = CI.lower, ymax = CI.upper), 
-                width = 0.2, 
-                position = position_dodge(width = 0.8),
-                size = 0.8) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
-  geom_label_repel(
-    aes(label = str_c(round(CATT, 1), '\n (', round(pct_change * 100, 1), '%)')),
-    position = position_dodge(width = 0.8), 
-    size = 3.5,
-    color = "black",
-    #nudge_y = 10,
-    segment.color = "grey70"
-  ) +
-  scale_fill_brewer(palette = "Set2") +
-  labs(
-    x = "Evaluation Time Point",
-    y = "Cumulative ATT",
-    title = "Cumulative ATT with Confidence Intervals at 1 and 3 Months",
-    subtitle = "Displaying differences by topic across time points",
-    fill = "Topic"
-  ) +
-  theme_minimal() +
-  theme(
-    plot.title = element_text(size = 14, face = "bold", hjust = 0.5),
-    plot.subtitle = element_text(size = 12, face = "italic", hjust = 0.5),
-    axis.text = element_text(size = 10),
-    axis.title = element_text(size = 12, face = "bold"),
-    legend.text = element_text(size = 10),
-    legend.title = element_text(size = 12, face = "bold"),
-    panel.grid.major = element_line(color = "grey90"),
-    panel.grid.minor = element_blank(),
-    legend.position = "top"
-  )
-ce_fig
-# ce_fig
-ggsave(filename = here(figures_dir, str_glue("pre-registered_cumulative_effects.png")), plot = ce_fig,width = 9,height = 9)
-
-
-plots <- lapply(1:4, function(i) {
-  results[[i]]$att_plot +
-    labs(
-      x = "Time (Weeks) Relative to Law Passage",  # New x-axis label
-      y = "ATT", # (Average Treatment Effect)",     # New y-axis label
-      title = str_glue("Estimated ATT Effect Over Time (Search Term {results[[i]]$search_term})") # Unique title for each plot
-    ) +
-    theme_minimal() # Optional: Use a clean theme
-})
-
-three_month_att<-plots[[1]]$data %>% filter(time>0,time<13) %>% summarise(mean(ATT)) %>% pull()
-# Assign individual plots for reference
-p1 <- plots[[1]] 
-p2 <- plots[[2]]
-p3 <- plots[[3]]
-p4 <- plots[[4]]
-
-# Combine plots using patchwork
-combined_plot <- p1 / p3 / p2 / p4 # Stack them vertically
-
-# Display the combined plot
-# print(combined_plot)
-
-# Save the combined plot
-ggsave(
-  filename = here(figures_dir, str_glue("pre_registered_specification_plot.png")),
-  plot = combined_plot,
-  width = 10,
-  height = 15
-)
-
-pre_registered_table<-ce_pre_registered%>% filter(rn==12) %>% mutate_at(c("CATT",'CI.lower','CI.upper','S.E.'), ~./13)
-
-
-pretreatment_fit<-
-  bind_rows(
-    results[[1]]$att_plot$data %>% filter(time<0) %>% summarise(pretreatment_difference=mean(abs(ATT)),topic=results[[1]]$search_term),
-    results[[2]]$att_plot$data %>% filter(time<0) %>% summarise(pretreatment_difference=mean(abs(ATT)),topic=results[[2]]$search_term),
-    results[[3]]$att_plot$data %>% filter(time<0) %>% summarise(pretreatment_difference=mean(abs(ATT)),topic=results[[3]]$search_term),
-    results[[4]]$att_plot$data %>% filter(time<0) %>% summarise(pretreatment_difference=mean(abs(ATT)),topic=results[[4]]$search_term)
-  )
-
-latex_table <- pretreatment_fit %>%
-  left_join(pre_registered_table, by = 'topic') %>%
-  select(topic, pretreatment_difference, CATT, CI.lower, CI.upper, p.value) 
-
-print(xtable(latex_table, caption = "Results Table", label = "tab:results"), include.rownames = FALSE)
-
-
-cat("\\begin{landscape}
-\\begin{table}[ht]
-\\centering\n")
-xtab_1<-xtable(results[[1]]$mod$wgt.implied)
-xtab_1
-results[[2]]$mod$wgt.implied %>% round(digits = 3) %>% xtable(caption='Model Weights (Gsynth)',label='vpn_weights')
-results[[3]]$mod$wgt.implied %>% round(digits = 3) %>% xtable(caption='Model Weights (Gsynth)',label='xvideos_weights',)
-results[[3]]$search_term
-oof<-results[[1]]$mod$wgt.implied  /colSums(results[[1]]$mod$wgt.implied)
-
-
-export_landscape_xtable <- function(data, filename = "table.tex",
-                                    caption = "Your Caption",
-                                    label = "your_label",
-                                    digits = NULL,
-                                    align = NULL) {
-  # Create xtable object
-  xtab <- xtable(data, caption = caption, label = label, digits = digits, align = align)
-  
-  # Capture the table body
-  tab_lines <- capture.output(
-    print(xtab,
-          include.rownames = TRUE,
-          floating = FALSE,
-          tabular.environment = "tabular",
-          hline.after = c(-1, 0, nrow(data)))
-  )
-  
-  # Combine with LaTeX wrappers
-  full_table <- c(
-    "\\begin{landscape}",
-    "\\begin{table}[ht]",
-    "\\centering",
-    "\\resizebox{\\textwidth}{!}{%",
-    tab_lines,
-    "}",
-    paste0("\\caption{", caption, "}"),
-    paste0("\\label{", label, "}"),
-    "\\end{table}",
-    "\\end{landscape}"
-  )
-  
-  # Write to file
-  writeLines(full_table, filename)
-}
